be0155c23caaef94559440107bc500e412f20c6d
biaSantana <danybsantana@gmail.com>
2024-05-23 21:55:09 -0300

    Projeto Criado

---
+++ src/main/java/bia/com/chess_system/ChessSystemApplication.java
@@ -0,0 +1,17 @@
+package bia.com.chess_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessSystemApplication.class, args);
+	
+	System.out.println("Hello World");
+	
+	}
+
+
+}
---
+++ src/test/java/bia/com/chess_system/ChessSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package bia.com.chess_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
