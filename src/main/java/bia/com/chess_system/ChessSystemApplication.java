585ff94e74f4f07584863a10b5724b819787cf68
biaSantana <danybsantana@gmail.com>
2024-05-23 22:13:28 -0300

    Primeira Classe criada: Position

--- src/main/java/bia/com/chess_system/ChessSystemApplication.java
+++ src/main/java/bia/com/chess_system/ChessSystemApplication.java
@@ -3,13 +3,17 @@ package bia.com.chess_system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import bia.com.chess_system.Boardgame.Position;
+
 @SpringBootApplication
 public class ChessSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChessSystemApplication.class, args);
 	
-	System.out.println("Hello World");
+	Position pos = new Position(3, 5);
+	
+		System.out.println(pos);
 	
 	}
 
