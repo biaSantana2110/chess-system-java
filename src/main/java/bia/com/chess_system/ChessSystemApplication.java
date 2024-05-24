f854ad34473866bca00f4f022d6ef4163ffb1cff
biaSantana <danybsantana@gmail.com>
2024-05-23 22:31:33 -0300

    Começando a Implementar as classes Board e Pieces

--- src/main/java/bia/com/chess_system/ChessSystemApplication.java
+++ src/main/java/bia/com/chess_system/ChessSystemApplication.java
@@ -3,7 +3,8 @@ package bia.com.chess_system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import bia.com.chess_system.Boardgame.Position;
+import bia.com.chess_system.Boardgame.Board;
+
 
 @SpringBootApplication
 public class ChessSystemApplication {
@@ -11,9 +12,7 @@ public class ChessSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ChessSystemApplication.class, args);
 	
-	Position pos = new Position(3, 5);
-	
-		System.out.println(pos);
+		Board board = new Board(8, 8);
 	
 	}
 
