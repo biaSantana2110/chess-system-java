916902571da5bb842c0dea65acb9250ca1e836ae
biaSantana <danybsantana@gmail.com>
2024-05-30 05:38:37 -0300

    Implementando os moviementos possíveis para a Torre/Rook(R)

--- src/main/java/com/bia/chess_system/chess/ChessPiece.java
+++ src/main/java/com/bia/chess_system/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package com.bia.chess_system.chess;
 
 import com.bia.chess_system.boardgame.Board;
 import com.bia.chess_system.boardgame.Piece;
+import com.bia.chess_system.boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
     private Color color;
@@ -14,5 +15,11 @@ public abstract class ChessPiece extends Piece {
     public Color getColor() {
         return this.color;
     }
+
+    protected boolean isThereOpponentPiece(Position position){
+        ChessPiece p = (ChessPiece)getBoard().piece(position);
+        return p !=null && p.getColor() !=color;
+    }
+
 
 }
