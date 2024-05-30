f6eacd5069b422a9460cab3303f1d1d78c08ec54
biaSantana <danybsantana@gmail.com>
2024-05-30 04:54:15 -0300

    Movimentos possíveis de uma peça

--- src/main/java/com/bia/chess_system/chess/ChessPiece.java
+++ src/main/java/com/bia/chess_system/chess/ChessPiece.java
@@ -3,7 +3,7 @@ package com.bia.chess_system.chess;
 import com.bia.chess_system.boardgame.Board;
 import com.bia.chess_system.boardgame.Piece;
 
-public class ChessPiece extends Piece {
+public abstract class ChessPiece extends Piece {
     private Color color;
 
     public ChessPiece(Board board, Color color) {
