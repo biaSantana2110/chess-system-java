ee65fef087d29e0135482648fe38e69c2489204f
biaSantana <danybsantana@gmail.com>
2024-06-05 17:58:43 -0300

    Contagem de moviemento das Peças

--- src/main/java/com/bia/chesssystem/chess/ChessPiece.java
+++ src/main/java/com/bia/chesssystem/chess/ChessPiece.java
@@ -6,6 +6,7 @@ import com.bia.chesssystem.boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
@@ -16,14 +17,25 @@ public abstract class ChessPiece extends Piece {
         return this.color;
     }
 
-    public ChessPosition getChessposition(){
-        return  ChessPosition.fromPosition(position);
+    public int getMoveCount() {
+        return moveCount;
     }
 
-    protected boolean isThereOpponentPiece(Position position){
-        ChessPiece p = (ChessPiece)getBoard().piece(position);
-        return p !=null && p.getColor() !=color;
+    public void increaseMoveCount() {
+        moveCount++;
     }
 
+    public void decreaseMoveCount() {
+        moveCount--;
+    }
+
+    public ChessPosition getChessposition() {
+        return ChessPosition.fromPosition(position);
+    }
+
+    protected boolean isThereOpponentPiece(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p != null && p.getColor() != color;
+    }
 
 }
